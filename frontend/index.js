a1a361309c937f4f3ec9134f76530a2df3a23017
Lamisha Pullom <163812146+Lpullom@users.noreply.github.com>
2024-06-13 16:15:15 -0500

    Add files via upload

--- frontend/index.js
+++ frontend/index.js
@@ -37,6 +37,10 @@ function moduleProject2() {
       row.appendChild(square)
       square.addEventListener('click', () => {
         // 👉 TASK 2 - Use a click handler to target a square 👈
+        if (!square.classList.contains('targeted')) {
+          document.querySelector('.targeted').classList.remove('targeted')
+        square.classList.add('targeted')  
+        }
       })
     }
   }
@@ -66,13 +70,75 @@ function moduleProject2() {
   document.addEventListener('keydown', evt => {
     // 👉 TASK 3 - Use the arrow keys to highlight a new square 👈
 
+    console.log(evt.key)
+
+    let isUp = evt.key === keys.up
+    let isDown = evt.key === keys.down
+    let isLeft = evt.key === keys.left
+    let isRight = evt.key === keys.right
+    let isSpacebar = evt.key === keys.space
+
+    let targeted = document.querySelector('.targeted')
+    
+    if (isUp) {
+      if (targeted.parentElement.previousElementSibling) {
+        let idx = Array.from(targeted.parentElement.children).indexOf(targeted)
+        targeted.classList.remove('targeted')
+        targeted.parentElement.previousElementSibling.children[idx].classList.add('targeted')
+  }
+    } else if (isDown) { 
+    if (targeted.parentElement.nextElementSibling) {
+      let idx = Array.from(targeted.parentElement.children).indexOf(targeted)
+      targeted.classList.remove('targeted')
+      targeted.parentElement.nextElementSibling.children[idx].classList.add('targeted')
+    }
+  }else if (isLeft) {
+    if (targeted.previousElementSibling) {
+        targeted.classList.remove('targeted')
+        targeted.previousElementSibling.classList.add('targeted')
+  }
+
+}else if (isRight) {
+    if (targeted.nextElementSibling) {
+      targeted.classList.remove('targeted')
+      targeted.nextElementSibling.classList.add('targeted')
+}
+  }
+
+   
+
     // 👉 TASK 4 - Use the space bar to exterminate a mosquito 👈
+   else if (isSpacebar) {
+     let mosquito = targeted.firstChild
+    
+     if (mosquito && mosquito.dataset.status === 'alive') {
+      mosquito.dataset.staus = 'dead'
+       mosquito.parentElement.style.backgroundColor = 'red'
+     
+      }
+    }
 
     // 👉 TASK 5 - End the game 👈
   })
   // 👆 WORK WORK ABOVE THIS LINE 👆
-}
 
+  let liveMosquitoes = document.querySelectorAll('[data-status=alive]')
+  if (!liveMosquitoes.length){
+    let elapsed = getTimeElapsed()
+    document.querySelector('p.info').textContent =
+    `Extermination completed in ${elapsed / 1000} seconds !`
+    
+    
+    let restartBtn = document.createElement('button')
+    restartBtn.textContent = 'Restart'
+    restartBtn.addEventListener('click', () => {
+      location.reload()
+    }) 
+    document.querySelector('h2').insertAdjacentElement('beforeend', restartBtn)
+    restartBtn.focus()
+  }
+  
+}
 // ❗ DO NOT MODIFY THE CODE BELOW
 // ❗ DO NOT MODIFY THE CODE BELOW
 // ❗ DO NOT MODIFY THE CODE BELOW
